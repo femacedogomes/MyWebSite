12d0a0abdb78e0fc9f5b4167b60f98dbd8789f43
Felipe Macedo Gomes <femacedogomes@gmail.com>
2022-10-25 13:42:47 -0300

    1.0

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectScreen from './screens/projects';
 import ContactScreen from './screens/contact';
 import AboutMeScreen from './screens/aboutMe';
@@ -8,7 +8,7 @@ import ThanksPage from './screens/Thanks';
 
 function RoutesNav() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path='/' element={<HomeScreen/>}/>
                 <Route path='/about' element={<AboutMeScreen/>}/>
@@ -16,7 +16,7 @@ function RoutesNav() {
                 <Route path='/contact' element={<ContactScreen/>}/>
                 <Route path='/thanks' element={<ThanksPage/>}/>
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
 
